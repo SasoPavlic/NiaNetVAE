e5540be3eaf430786a0751711948a4a3a5c0110f
SasoPavlic <saso.pavlic3@gmail.com>
2022-07-21 17:17:10 +0200

    Demo

--- models/lstm_vae.py
+++ models/lstm_vae.py
@@ -1,3 +1,6 @@
+from random import random
+
+import numpy as np
 import torch
 from .base import BaseVAE
 from .types_ import *
@@ -9,37 +12,75 @@ import torch.distributions
 torch.manual_seed(0)
 
 
-class LSTMVAE(BaseVAE):
+class LSTMVAE(BaseVAE, nn.Module):
     def __init__(self,
                  seq_len,
                  n_features,
                  embedding_dim,
+                 solution=[0.1, 0.29, 0.7, 0.1, 0.1, 0.1, 0.1],
+                 dataset_shape=[1, 140],
                  **kwargs) -> None:
         super(LSTMVAE, self).__init__()
 
-        self.seq_len = seq_len
-        self.n_features = n_features
-        self.embedding_dim = embedding_dim
-        self.hidden_dim = 2 * embedding_dim
+        """
+        Dimensionality:
+            y1: topology shape,
+            y2: number of neurons per layer
+            y3: number of layers,
+            y4: activation function
+            y5: number of epochs,
+            y6: learning rate
+            y7: optimizer algorithm.
+        """
+
+        self.encoding_layers = nn.ModuleList()
+        self.decoding_layers = nn.ModuleList()
+
+        self.bottleneck_size = embedding_dim
+        self.shape = self.get_shape(solution[0])
+        self.layer_step = self.get_layer_step(solution[1], dataset_shape)
+        self.layers = self.get_layers(solution[2], self.layer_step, dataset_shape)
+        self.activation = self.get_activation(solution[3])
+        self.epochs = self.get_epochs(solution[4])
+        self.learning_rate = self.get_learning_rate(solution[5])
+
+        # TODO make it work
+        self.generate_autoencoder(self.shape,
+                                  self.layers,
+                                  dataset_shape,
+                                  self.layer_step)
+        #
+        # self.optimizer = self.get_optimizer(solution[6])
+
+        self.n_features = dataset_shape[0]
+        self.seq_len = dataset_shape[1]
+        self.embedding_dim = self.bottleneck_size
+        self.hidden_dim = 2 * self.bottleneck_size
 
         self.encoder_rnn1 = nn.LSTM(
-            input_size=n_features,
-            hidden_size=self.hidden_dim,
-            num_layers=n_features,
+            input_size=1,
+            hidden_size=128,
+            num_layers=1,
             batch_first=True
         )
 
         self.encoder_rnn2 = nn.LSTM(
-            input_size=self.hidden_dim,
-            hidden_size=embedding_dim,
-            num_layers=n_features,
+            input_size=128,
+            hidden_size=80,
+            num_layers=1,
+            batch_first=True
+        )
+
+        self.encoder_rnn3 = nn.LSTM(
+            input_size=80,
+            hidden_size=64,
+            num_layers=1,
             batch_first=True
         )
 
         self.fc_mu = nn.Linear(self.embedding_dim, self.embedding_dim)
         self.fc_var = nn.Linear(self.embedding_dim, self.embedding_dim)
 
-        self.seq_len = seq_len
         self.input_dim = self.embedding_dim
         self.hidden_dim = 2 * self.embedding_dim
         self.n_features = self.n_features
@@ -53,14 +94,21 @@ class LSTMVAE(BaseVAE):
 
         self.decoder_rnn2 = nn.LSTM(
             input_size=self.input_dim,
-            hidden_size=self.hidden_dim,
+            hidden_size=80,
             num_layers=self.n_features,
             batch_first=True
         )
 
-        self.decoder_output_layer = nn.Linear(self.hidden_dim, seq_len)
+        self.decoder_rnn3 = nn.LSTM(
+            input_size=80,
+            hidden_size=128,
+            num_layers=self.n_features,
+            batch_first=True
+        )
 
-    def encode(self, input: Tensor) -> List[Tensor]:
+        self.decoder_output_layer = nn.Linear(self.hidden_dim, self.seq_len)
+
+    def encode(self, x: Tensor) -> List[Tensor]:
         """
         Encodes the input by passing through the encoder network
         and returns the latent codes.
@@ -70,14 +118,18 @@ class LSTMVAE(BaseVAE):
 
         # input = Tensor (140, 1)
 
-        x = input.reshape((1, self.seq_len, self.n_features))
+        # batch_size=1, seq_len=140, n_features=1
+        x = x.reshape((1, self.seq_len, self.n_features))
         # x = Tensor(1,140,1)
 
-        x, (_, _) = self.encoder_rnn1(x)
+        x, (hidden_n, cell_n) = self.encoder_rnn1(x)
         # x = Tensor(140,256)
 
-        x, (hidden_n, _) = self.encoder_rnn2(x)
+        x, (hidden_n, cell_n) = self.encoder_rnn2(x)
         # hidden_n = Tensor(140, 128)
+
+        x, (hidden_n, cell_n) = self.encoder_rnn3(x)
+        # hidden_n = Tensor(140, 64)
 
         # TODO Why hidden state needs to be passed
         # https://github.com/chrisvdweth/ml-toolkit\
@@ -85,14 +137,12 @@ class LSTMVAE(BaseVAE):
         hidden_n = hidden_n.reshape((self.n_features, self.embedding_dim))
         # hidden_n = Tensor(1, 128)
 
-        result = hidden_n
-
         # Split the result into mu and var components
         # of the latent Gaussian distribution
-        mu = self.fc_mu(result)
+        mu = self.fc_mu(hidden_n)
         # mu = Tensor(1, 128)
 
-        log_var = self.fc_var(result)
+        log_var = self.fc_var(hidden_n)
         # log_var = Tensor(1, 128)
 
         return [mu, log_var]
@@ -110,17 +160,20 @@ class LSTMVAE(BaseVAE):
         x = z.reshape((self.n_features, self.input_dim))
         # x = Tensor (1, 128)
 
-        out, states = self.decoder_rnn1(x)
+        x, (hidden_n, cell_n) = self.decoder_rnn1(x)
         # out = Tensor (1, 128)
 
-        out, states = self.decoder_rnn2(out)
+        x, (hidden_n, cell_n) = self.decoder_rnn2(x)
         # out = Tensor (1, 256)
 
-        x = out.reshape((self.n_features, self.hidden_dim))
+        x, (hidden_n, cell_n) = self.decoder_rnn3(x)
+        # x = Tensor(1, 128)
+
+        x = x.reshape((self.n_features, self.hidden_dim))
         # x = Tensor (1, 256)
-        result = self.decoder_output_layer(x)
+        reconstructed = self.decoder_output_layer(x)
         # result = Tensor (1, 140)
-        return result
+        return reconstructed
 
     def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
         """
@@ -151,10 +204,10 @@ class LSTMVAE(BaseVAE):
 
         input = input.reshape(input.shape[1], input.shape[0])
         # input = Tensor(1, 140)
-        result = self.decode(z)
-        # result = Tensor(1, 140)
+        reconstructed = self.decode(z)
+        # reconstructed = Tensor(1, 140)
 
-        return [result, input, mu, log_var]
+        return [reconstructed, input, mu, log_var]
 
     def loss_function(self,
                       *args,
@@ -205,3 +258,217 @@ class LSTMVAE(BaseVAE):
         """
         reconstructed = self.forward(x)[0]
         return reconstructed
+
+    def get_shape(self, gene):
+        gene = np.array([gene])
+        bins = np.array([0.0, 0.5])
+        inds = np.digitize(gene, bins)
+
+        if inds[0] - 1 == 0:
+            return "SYMMETRICAL"
+
+        elif inds[0] - 1 == 1:
+            return "A-SYMMETRICAL"
+
+        else:
+            raise ValueError(f"Value not between boundaries 0.0 and 1.0. Value is: {inds[0] - 1}")
+
+    def get_layer_step(self, gene, dataset_shape):
+        gene = np.array([gene])
+        bins = []
+        value = 1 / dataset_shape[1]
+        step = value
+        for col in range(0, dataset_shape[1]):
+            bins.append(step)
+            step += value
+        bins[-1] = 1.01
+        inds = np.digitize(gene, bins)
+        return inds[0]
+
+    def get_layers(self, gene, layer_step, dataset_shape):
+        if layer_step == 0:
+            max_layers = dataset_shape[1]
+            return max_layers
+
+        else:
+            max_layers = round(dataset_shape[1] / layer_step)
+
+        if max_layers == 1:
+            return 1
+
+        else:
+            gene = np.array([gene])
+
+            bins = []
+            value = 1 / max_layers
+            step = value
+            for col in range(0, max_layers):
+                bins.append(step)
+                step += value
+            bins[-1] = 1.01
+            inds = np.digitize(gene, bins)
+
+            return inds[0]
+
+    def get_activation(self, gene):
+        gene = np.array([gene])
+        bins = np.array([0.0, 0.125, 0.25, 0.375, 0.500, 0.625, 0.750, 0.875, 1.01])
+        inds = np.digitize(gene, bins)
+
+        if inds[0] - 1 == 0:
+            return F.elu
+
+        elif inds[0] - 1 == 1:
+            return F.relu
+
+        elif inds[0] - 1 == 2:
+            return F.leaky_relu
+
+        elif inds[0] - 1 == 3:
+            return F.rrelu
+
+        elif inds[0] - 1 == 4:
+            return F.selu
+
+        elif inds[0] - 1 == 5:
+            return F.celu
+
+        elif inds[0] - 1 == 6:
+            return F.gelu
+
+        elif inds[0] - 1 == 7:
+            return torch.tanh
+
+        else:
+
+            raise ValueError(f"Value not between boundaries 0.0 and 1.0. Value is: {inds[0] - 1}")
+
+    def get_epochs(self, gene):
+        gene = np.array([gene])
+        bins = np.array([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.60, 0.7, 0.8, 0.9, 1.01])
+        inds = np.digitize(gene, bins)
+
+        return inds[0] * 10 + 100
+
+    def get_learning_rate(self, gene):
+        gene = np.array([gene])
+        bins = []
+        value = 1 / 1000
+        step = value
+        for col in range(0, 1000):
+            bins.append(step)
+            step += value
+        bins[-1] = 1.01
+        inds = np.digitize(gene, bins)
+        lr = np.array(bins)[inds[0]]
+
+        return lr
+
+    def generate_autoencoder(self, shape, layers, dataset_shape, layer_step):
+        if shape == "SYMMETRICAL":
+
+            i = dataset_shape[1]
+            z = dataset_shape[1] - layer_step
+
+            while layers != 0:
+                """Minimum depth reached"""
+                if z < 1:
+                    self.encoding_layers.append(nn.Linear(in_features=i, out_features=z + 1))
+                    self.decoding_layers.insert(0, nn.Linear(in_features=z + 1, out_features=i))
+                    self.bottleneck_size = z + 1
+                    break
+
+                self.encoding_layers.append(nn.Linear(in_features=i, out_features=z))
+                self.decoding_layers.insert(0, nn.Linear(in_features=z, out_features=i))
+                i = i - layer_step
+                z = z - layer_step
+                layers = layers - 1
+
+            if len(self.encoding_layers) == 0:
+                self.bottleneck_size = 0
+            else:
+                self.bottleneck_size = self.encoding_layers[-1].out_features
+
+        elif shape == "A-SYMMETRICAL":
+            i = dataset_shape[1]
+            z = dataset_shape[1] - layer_step
+
+            if layers == 1 or layers == 2:
+                self.encoding_layers.append(nn.Linear(in_features=i, out_features=z))
+                self.decoding_layers.insert(0, nn.Linear(in_features=z, out_features=i))
+
+            if layers >= 3:
+                layers_encoder = random.randint(1, layers)
+                layers_decoder = layers - layers_encoder
+
+                encoder_counter = layers_encoder
+                decoder_counter = layers_decoder
+
+                if layers_decoder == 0:
+                    layers_encoder = layers_encoder - 1
+                    layers_decoder = 1
+
+                    encoder_counter = layers_encoder
+                    decoder_counter = layers_decoder
+
+                while encoder_counter != 0:
+
+                    if z < 1:
+                        self.encoding_layers.append(nn.Linear(in_features=i, out_features=z + 1))
+                        self.bottleneck_size = z + 1
+                        break
+
+                    self.encoding_layers.append(nn.Linear(in_features=i, out_features=z))
+
+                    i = i - layer_step
+                    z = z - layer_step
+                    encoder_counter = encoder_counter - 1
+
+                while decoder_counter != 0:
+
+                    if layers_decoder == 1:
+                        self.decoding_layers.insert(0, nn.Linear(in_features=i, out_features=dataset_shape[1]))
+                        break
+
+                    layer_step = int((dataset_shape[1] - i) / decoder_counter)  # Make more complex logic
+                    last_i = i
+                    i = i + layer_step
+                    z = z + layer_step
+                    decoder_counter = decoder_counter - 1
+
+                    self.decoding_layers.append(nn.Linear(in_features=last_i, out_features=i))
+
+            if len(self.encoding_layers) == 0:
+                self.bottleneck_size = 0
+            else:
+                self.bottleneck_size = self.encoding_layers[-1].out_features
+
+    def get_optimizer(self, gene):
+        gene = np.array([gene])
+        bins = np.array([0.0, 0.167, 0.334, 0.50, 0.667, 0.834, 1.01])
+        inds = np.digitize(gene, bins)
+
+        """When AE does not have any layers"""
+        if len(list(self.parameters())) == 0:
+            return None
+
+        if inds[0] - 1 == 0:
+            return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+        elif inds[0] - 1 == 1:
+            return torch.optim.Adagrad(self.parameters(), lr=self.learning_rate)
+
+        elif inds[0] - 1 == 2:
+            return torch.optim.SGD(self.parameters(), lr=self.learning_rate)
+
+        elif inds[0] - 1 == 3:
+            return torch.optim.RAdam(self.parameters(), lr=self.learning_rate)
+
+        elif inds[0] - 1 == 4:
+            return torch.optim.ASGD(self.parameters(), lr=self.learning_rate)
+
+        elif inds[0] - 1 == 5:
+            return torch.optim.Rprop(self.parameters(), lr=self.learning_rate)
+
+        else:
+            raise ValueError(f"Value not between boundaries 0.0 and 1.0. Value is: {inds[0] - 1}")
