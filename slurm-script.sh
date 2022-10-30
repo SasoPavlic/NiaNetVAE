dae2dc486b6c6210bd389e39f364b243881d61cf
SasoPavlic <saso.pavlic3@gmail.com>
2022-10-30 22:33:01 +0100

    Updated SLURM script

--- slurm-script.sh
+++ slurm-script.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cores-per-socket=16
-#SBATCH --mem=8G
+#SBATCH --mem=40G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:4
-#SBATCH --time=8:00:00
+#SBATCH --time=48:00:00
 
-singularity exec -e --pwd /app -B ./logs:/app/logs --nv docker://spartan300/nianet:latest python ./rnn_vae_run.py
+singularity exec -e --pwd /app -B /ceph/grid/home/sasop/logs:/app/logs --nv docker://spartan300/nianet:latest python ./rnn_vae_run.py
