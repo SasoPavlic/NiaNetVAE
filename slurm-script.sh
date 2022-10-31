58acd043da2843d2a20a4a155f5aedf9c2b8afbe
SasoPavlic <saso.pavlic3@gmail.com>
2022-10-31 13:43:09 +0100

    Updated slurm-script.sh to work with one-gpu

--- slurm-script.sh
+++ slurm-script.sh
@@ -4,10 +4,9 @@
 #SBATCH -e nianet-%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cores-per-socket=16
-#SBATCH --mem=40G
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:4
+#SBATCH --mem-per-gpu=32GB
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
 
 singularity exec -e --pwd /app -B /ceph/grid/home/sasop/logs:/app/logs --nv docker://spartan300/nianet:latest python ./rnn_vae_run.py
